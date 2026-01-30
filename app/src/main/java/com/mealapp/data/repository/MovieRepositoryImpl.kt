3ce45c9b2e5e2840ac12b90bcae434f015b54ed5
Maxime Bodin <maxime.bodin@mailo.com>
2026-01-30 18:20:24 +0100

    Adding repository.

---
+++ app/src/main/java/com/mealapp/data/repository/MovieRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.mealapp.data.repository
+
+import com.mealapp.data.network.ApiClient
+import com.mealapp.data.network.TheMealDbApiService
+import com.mealapp.data.network.toDomain
+import com.mealapp.domain.model.Category
+import com.mealapp.domain.model.Meal
+import com.mealapp.domain.model.MealDetails
+import com.mealapp.domain.repository.MealRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+
+
+class MealRepositoryImpl(
+    private val apiService: TheMealDbApiService = ApiClient.create()
+) : MealRepository {
+
+    override fun getCategories(): Flow<Result<List<Category>>> = flow {
+        val response = apiService.getCategories().categories
+
+        emit(Result.success(response.map { it.toDomain() }))
+    }.catch { throwable ->
+        emit(Result.failure(throwable))
+    }
+
+    override fun getMealsByCategory(category: String): Flow<Result<List<Meal>>> = flow {
+        val response = apiService.getMealsByCategory(category).meals
+
+        emit(Result.success(response.map { it.toDomain() }))
+    }.catch { throwable ->
+        emit(Result.failure(throwable))
+    }
+
+    override fun getMealDetails(id: String): Flow<Result<MealDetails?>> = flow {
+        val response = apiService.getMealDetails(id).meals.firstOrNull()
+
+        emit(Result.success(response?.toDomain()))
+    }.catch { throwable ->
+        emit(Result.failure(throwable))
+    }
+}
